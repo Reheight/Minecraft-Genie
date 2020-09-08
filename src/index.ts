3a1bdfb954b17b25bf8421ce0f002c0b09521e6f
Reheight <dustinpalmatier@icloud.com>
2020-09-08 13:46:17 -0400

    --- Auto-Git Commit ---

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,23 @@ import BotClient from "./client/client";
 const client: BotClient = new BotClient({ token, owners });
 
 client.start();
+
+const express = require('express');
+var cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const service = app.listen(23224, () => {
+    console.log(`API is now active on port: ${service.address().port}`);
+});
+
+app.set('json spaces', 2);
+
+app.get('/api', (req, res) => {
+    let response = {
+        online: true
+    }
+    
+    res.json(response);
+});
